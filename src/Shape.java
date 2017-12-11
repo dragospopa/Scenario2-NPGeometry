2e9fa4453111207968bf94d5ce77045aa63d991e
Tomasz Czernuszenko <tczernuszenko@gmail.com>
2017-12-11 17:21:18 +0000

    created the shape class

---
+++ src/Shape.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+/**
+ * Created by tomaszczernuszenko on 11/12/2017.
+ */
+public class Shape {
+    ArrayList<Coordinate> vertices;
+
+    public Shape(ArrayList<Coordinate> vertices) {
+        this.vertices = vertices;
+    }
+
+    public ArrayList<Coordinate> rotate(double degrees){
+
+    }
+
+    public ArrayList<Coordinate> convexHull(){
+
+    }
+
+    public ArrayList<Coordinate> rectangleOver(){
+
+    }
+
+    public double area(){
+
+    }
+
+}
