0d31a7911b83a53aa6f82fee218a295c3f928ebe
Oscar King <zcaboki@ucl.ac.uk>
2017-12-14 17:07:13 +0000

    might run a little slower, saving the best scoring arrangement in currentMax

--- src/TaskThread.java
+++ src/TaskThread.java
@@ -5,22 +5,25 @@
 public class TaskThread extends Thread {
 
     CaseSolver solver;
+    CaseSolver currentMax;
     int order;
     JobDoneListener listener;
 
     public TaskThread(CaseSolver solver, int order, JobDoneListener listener){
         this.solver = solver;
+        this.currentMax = solver;
         this.order = order;
         this.listener = listener;
     }
 
     public void run() {
         int attempts=0;
-        do {
-            this.solver = new CaseSolver(this.solver.room, this.solver.decorations);
+        while (attempts < 20) {
             solver.solve(order);
+            currentMax = ((solver.getTotalCost()>currentMax.getTotalCost()&&(solver.getCoverage()>30)) ? solver:currentMax;
+            this.solver = new CaseSolver(this.solver.room, this.solver.decorations);
             attempts++;
-        } while (solver.getCoverage() < 30 && attempts < 20);
+        }
         listener.notifyJobDone(extractResult(), order);
     }
 
